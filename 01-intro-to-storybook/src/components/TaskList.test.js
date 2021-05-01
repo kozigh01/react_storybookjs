6ccd22c28e4a012cef9dc88da8e9c4c2cba18ddc
Mark Kozisek <mark.kozisek@mastery.net>
2021-05-01 17:31:24 -0500

    Composite component with testing

---
+++ 01-intro-to-storybook/src/components/TaskList.test.js
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+
+import { composeStories } from '@storybook/testing-react';
+
+import * as TaskListStories from './TaskList.stories';
+
+const { Default, WithPinnedTasks } = composeStories(TaskListStories);
+
+it('renders pinned tasks at the start of the list', () => {
+    const { container } = render(<WithPinnedTasks />);
+
+    expect(
+        container.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]')
+    ).not.toBe(null);
+});
+
+it('renders pinned tasks at the start of the list', () => {
+    const { container } = render(<Default />);
+
+    expect(
+        container.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]')
+    ).toBe(null);
+});
